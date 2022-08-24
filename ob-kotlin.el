1cd7c041852651715e1272ba872a2c64e69d4f48
John Herrlin <jherrlin@gmail.com>
2022-08-24 08:07:38 +0200

    insert "fun main" if missing

--- ob-kotlin.el
+++ ob-kotlin.el
@@ -29,6 +29,65 @@ path name, like /usr/local/bin/kotlinc."
   :package-version '(Org . "9.5")
   :type 'string)
 
+(defconst org-babel-kotlin--main-fun-re
+  (rx line-start (0+ space) "fun"
+      (1+ space) "main()")
+  "Regexp for the main method declaration.")
+
+(defconst org-babel-kotlin--package-re (rx line-start (0+ space) "package"
+					 (1+ space) (group (1+ (in alnum ?_ ?.))) ; capture the package name
+					 (0+ space) ?\; line-end)
+  "Regexp for the package statement.")
+
+(defconst org-babel-kotlin--package-re (rx line-start (0+ space) "package"
+					 (1+ space) (group (1+ (in alnum ?_ ?.))) ; capture the package name
+					 (0+ space) ?\; line-end)
+  "Regexp for the package statement.")
+
+(defconst org-babel-kotlin--imports-re (rx line-start (0+ space) "import"
+                                         (opt (1+ space) "static")
+					 (1+ space) (group (1+ (in alnum ?_ ?. ?*))) ; capture the fully qualified class name
+					 (0+ space) ?\; line-end)
+  "Regexp for import statements.")
+
+(defun org-babel-kotlin--move-past (re)
+  "Move point past the first occurrence of the given regexp RE."
+  (while (re-search-forward re nil t)
+    (goto-char (1+ (match-end 0)))))
+
+(defun org-babel-expand-body:kotlin (body params)
+  "Expand BODY with PARAMS.
+BODY could be a few statements, or could include a full class
+definition specifying package, imports, and class.  Because we
+allow this flexibility in what the source block can contain, it
+is simplest to expand the code block from the inside out."
+  (let* ((imports-val (assq :imports params))
+         (imports (if imports-val
+                      (split-string (org-babel-read (cdr imports-val) nil) " ")
+                    nil)))
+    (with-temp-buffer
+      (insert body)
+
+      ;; wrap main.  If there are methods defined, but no main method
+      ;; and no class, wrap everything in a generic main method.
+      (goto-char (point-min))
+      (when (not (re-search-forward org-babel-kotlin--main-fun-re nil t))
+        (org-babel-kotlin--move-past org-babel-kotlin--package-re) ; if package is defined, move past it
+        (org-babel-kotlin--move-past org-babel-kotlin--imports-re) ; if imports are defined, move past them
+        (insert "fun main() {\n")
+        (indent-code-rigidly (point) (point-max) 4)
+        (goto-char (point-max))
+        (insert "\n}"))
+
+      ;; add imports from source block headers
+      (when imports
+        (goto-char (point-min))
+        (org-babel-kotlin--move-past org-babel-kotlin--package-re) ; if package is defined, move past it
+        (insert (mapconcat (lambda (package) (concat "import " package ";")) imports "\n") "\n"))
+
+      ;; return expanded body
+      (buffer-string))))
+
 (defun org-babel-execute:kotlin (body params)
   "Execute a kotlin source block with BODY code and PARAMS params."
   (message body)
@@ -37,7 +96,7 @@ path name, like /usr/local/bin/kotlinc."
          (result-params (cdr (assq :result-params params)))
          (classpath (cdr (assq :classpath params)))
          (filename "main.kt")
-         (full-body body)
+         (full-body (org-babel-expand-body:kotlin body params))
          (compile-command
           (concat ob-kotlin:kotlinc " " filename " "
                   (when classpath
@@ -47,17 +106,21 @@ path name, like /usr/local/bin/kotlinc."
           (concat org-babel-kotlin-command
                   " -classpath main.jar"
                   (when classpath
-                    (concat ":" classpath " "))
-                  "MainKt"))
+                    (concat ":" classpath))
+                  " MainKt"))
          (cmd (concat
                compile-command
                " && "
                run-command)))
-    (message "Inserting code into:" filename)
-    (message "Running:" cmd)
+    (message filename)
+    (message cmd)
     (with-temp-file filename (insert full-body))
     ;; compile, run, process result
     (org-babel-reassemble-table
      (org-babel-java-evaluate cmd result-type result-params nil)
      (org-babel-pick-name nil nil)
      (org-babel-pick-name nil nil))))
+
+(provide 'ob-kotlin)
+
+;;; ob-kotlin.el ends here
